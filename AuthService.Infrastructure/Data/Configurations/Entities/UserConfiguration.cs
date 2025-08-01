82f5e3c38e9f7017d496cd7e53db2e972a15f21b
RubenFern <rubenferval@hotmail.com>
2025-08-01 18:07:35 +0200

    refactor identation

--- AuthService.Infrastructure/Data/Configurations/Entities/UserConfiguration.cs
+++ AuthService.Infrastructure/Data/Configurations/Entities/UserConfiguration.cs
@@ -12,36 +12,46 @@ public class UserConfiguration : EntityConfiguration<User>
         base.Configure(builder);
         
         builder.ToTable("User");
+        
         builder.HasOne(d => d.Role)
             .WithMany(p => p.Users)
             .HasForeignKey(d => d.RoleId);
+        
         builder.HasOne(d => d.Status)
             .WithMany(p => p.Users)
             .HasForeignKey(d => d.StatusId);
+        
         builder.Property(e => e.Username)
             .IsRequired()
             .HasMaxLength(50)
             .HasColumnName("username");
+        
         builder.Property(e => e.Email)
             .IsRequired()
             .HasMaxLength(100)
             .HasColumnName("email");
+        
         builder.Property(e => e.Password)
             .IsRequired()
             .HasMaxLength(255)
             .HasColumnName("password");
+        
         builder.Property(e => e.FirstName)
             .HasMaxLength(50)
             .HasColumnName("first_name");
+        
         builder.Property(e => e.LastName)
             .HasMaxLength(50)
             .HasColumnName("last_name");
+        
         builder.Property(e => e.DateOfBirth)
             .HasColumnType("date")
             .HasColumnName("date_of_birth");
+        
         builder.Property(e => e.LastLogin)
             .HasColumnType("timestamp")
             .HasColumnName("last_login");
+        
         builder.Property(e => e.ProfilePictureUrl)
             .HasMaxLength(255)
             .HasColumnName("profile_picture_url");
